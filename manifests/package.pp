1b2ac6702dd0a4756c29f0d622ade5cc0d856df8
Nathan Valentine <nathan@puppetlabs.com>
2013-04-05 14:46:03 -0700

    most of the way toward supporting RedHat but some issues with package and assumed uid

--- manifests/package.pp
+++ manifests/package.pp
@@ -2,14 +2,12 @@ class gitolite::package (
   $path,
   $user,
   $group = $user,
-  $admin_pub_key
+  $admin_pub_key,
+  $enable_third_party_package_repo = true
 ) {
 
   include gitolite::package::params
 
-  $preseed = $gitolite::package::params::preseed
-  $package = $gitolite::package::params::package
- 
   user { $user: 
     ensure => present, 
     system => true, 
@@ -19,24 +17,65 @@ class gitolite::package (
 
   group { $group: ensure => present, system => true, }
 
-  file { $path: 
-    ensure => directory,
-    owner => $user,
-    group => $user,
-    mode => '0755',
+  $package = $gitolite::package::params::package
+
+  ##
+  ## Debian
+  ##
+
+  if 'debian' == $::osfamily {
+    $preseed = $gitolite::package::params::preseed
+
+    file { $path: 
+      ensure => directory,
+      owner => $user,
+      group => $user,
+      mode => '0755',
+    }
+
+    file { 'gitolite::package preseed':
+      path => $preseed,
+      owner => 'root',
+      group => 'root',
+      mode => '0600',
+      content => template('gitolite/Debian/gitolite.preseed.erb'),
+    }
+
+    package { $package:
+      ensure => present,
+      responsefile => $preseed,
+      require => [File[$preseed, $path], User[$user]],
+    }
   }
 
-  file { 'gitolite::package preseed':
-    path => $preseed,
-    owner => 'root',
-    group => 'root',
-    mode => '0600',
-    content => template('gitolite/Debian/gitolite.preseed.erb'),
-  }
+  ##
+  ## RedHat
+  ## 
 
-  package { $package:
-    ensure => present,
-    responsefile => $preseed,
-    require => File[$preseed, $path],
+  if ('redhat' == $::osfamily) {
+    if (true == $enable_third_party_package_repo) {
+      package { 'epel-release':
+        ensure => present,
+        provider => 'rpm',
+	source => $gitolite::package::params::epel_url,
+      } -> yumrepo { 'epel': enabled => '1', }
+    }
+
+    file { $path: ensure => symlink, target => '/var/lib/gitolite', }
+
+    package { $package: ensure => present, }
+
+    file { $gitolite::package::params::admin_pub_key_file:
+      owner => 'gitolite',
+      group => 'gitolite',
+      content => $admin_pub_key,
+      require => Package[$package],
+    }
+
+    exec { 'configure admin access to gitolite':
+      path => ['/bin','/sbin','/usr/bin','/usr/sbin'],
+      command => "gl-setup $gitolite::package::params::admin_pub_key_file",
+      require => [Package[$package], File[$gitolite::package::params::admin_pub_key_file]],
+    }
   }
 }
